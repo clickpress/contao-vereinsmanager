687e4a1b9bec5f6f0092ea89c1249d0dd0b816a2
Stefan Schulz-Lauterbach <ssl@clickpress.de>
2025-02-26 14:58:14 +0200

    Rename class ClickpressVereinsmanagerExtension to ContaoVereinsmanagerExtension
    Refactored the class name to align with the naming conventions and project structure. This ensures consistent naming and avoids potential conflicts or confusion within the codebase. No functional changes were introduced.

--- src/DependencyInjection/ContaoVereinsmanagerExtension.php
+++ src/DependencyInjection/ContaoVereinsmanagerExtension.php
@@ -6,7 +6,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
-class ClickpressVereinsmanagerExtension extends Extension
+class ContaoVereinsmanagerExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container): void
     {
